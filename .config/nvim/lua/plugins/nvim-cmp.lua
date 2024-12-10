3132af3e7e13eacb7101884b7eacb952ba7d4a07
Anthony M <38088724+Creeperman1524@users.noreply.github.com>
2024-12-09 19:06:04 -0500

    Updated nvim autocomplete
    Added ghost text and better keybinds for nvim autocomplete

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -4,6 +4,7 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for file system paths
+		"hrsh7th/cmp-nvim-lua", -- autocompletion for nvim plugins
 		{
 			"L3MON4D3/LuaSnip",
 			-- follow latest release.
@@ -27,11 +28,20 @@ return {
 			completion = {
 				completeopt = "menu,menuone,preview,noselect",
 			},
+
+			window = {
+				completion = {
+					scrollbar = false,
+				},
+			},
+
 			snippet = { -- configure how nvim-cmp interacts with snippet engine
 				expand = function(args)
 					luasnip.lsp_expand(args.body)
 				end,
 			},
+
+			-- configure all the key mappings for nvim-cmp
 			mapping = cmp.mapping.preset.insert({
 				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
@@ -39,13 +49,45 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				-- allow tab to be used to cycle through the menu items or the snippet areas
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif require("luasnip").expand_or_jumpable() then
+						vim.fn.feedkeys(
+							vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
+							""
+						)
+					else
+						fallback()
+					end
+				end, {
+					"i",
+					"s",
+				}),
+
+				-- allow shift tab to be used to cycle through the menu items or the snippet areas
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif require("luasnip").jumpable(-1) then
+						vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+					else
+						fallback()
+					end
+				end, {
+					"i",
+					"s",
+				}),
 			}),
+
 			-- sources for autocompletion (in order of how they show up)
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
+				{ name = "nvim-lua" }, -- nvim lua snippets
 				{ name = "path" }, -- file system paths
 			}),
 
@@ -55,6 +97,11 @@ return {
 					maxwidth = 50,
 					ellipsis_char = "...",
 				}),
+			},
+
+			-- adds the ghost text to the autocompletion
+			experimental = {
+				ghost_text = true,
 			},
 		})
 	end,
