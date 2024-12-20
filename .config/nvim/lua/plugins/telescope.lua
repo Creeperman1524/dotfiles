313c84f00f650ec6366435c797ad57f6bd8c4dff
Anthony M <38088724+Creeperman1524@users.noreply.github.com>
2024-12-20 15:53:30 -0500

    Updated telescope plugin
    Added a new 'search vim tags' options for telescope, and included the fzf package

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
 	branch = "0.1.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		-- { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		"nvim-tree/nvim-web-devicons",
 		"folke/todo-comments.nvim",
 	},
@@ -22,9 +22,12 @@ return {
 					},
 				},
 			},
+			extensions = {
+				fzf = {},
+			},
 		})
 
-		-- telescope.load_extension("fzf")
+		telescope.load_extension("fzf")
 		telescope.load_extension("notify")
 
 		-- set keymaps
@@ -34,6 +37,7 @@ return {
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+		keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Fuzzy find vim help tags" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 		keymap.set("n", "<leader>fn", "<cmd>Telescope notify<cr>", { desc = "Search notifications" })
 	end,
