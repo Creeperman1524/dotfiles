8ba5f32c593fdcebf8a0c3f270092f9598985665
Creeperman1524 <38088724+Creeperman1524@users.noreply.github.com>
2025-01-16 17:26:25 -0500

    Updated fonts and themes
    Updated the terminal font along with many themeing options for nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,14 +3,35 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme catppuccin-mocha")
-
 		require("catppuccin").setup({
+			flavour = "macchiato",
+			term_colors = true,
+			transparent_background = true,
+			styles = {
+				functions = { "italic" },
+				keywords = { "bold" },
+				comments = { "italic" },
+			},
 			integrations = {
+				alpha = true,
+				gitsigns = true,
 				notify = true,
 				noice = true,
 				mason = true,
+				telescope = {
+					enabled = true,
+					style = "nvchad",
+				},
+			},
+			color_overrides = {
+				mocha = {
+					base = "#000000",
+					crust = "#000000",
+					mantle = "#000000",
+				},
 			},
 		})
+
+		vim.cmd.colorscheme("catppuccin")
 	end,
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -14,12 +14,22 @@ return {
 		telescope.setup({
 			defaults = {
 				path_display = { "smart" },
+				prompt_prefix = "   ",
+				sorting_strategy = "ascending",
 				mappings = {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
 						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 					},
+				},
+				layout_config = {
+					horizontal = {
+						prompt_position = "top",
+						preview_width = 0.55,
+					},
+					width = 0.87,
+					height = 0.80,
 				},
 			},
 			extensions = {
@@ -33,12 +43,12 @@ return {
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-		keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Fuzzy find vim help tags" })
-		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files in cwd" })
+		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Find recent files" })
+		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Grep a string in cwd" })
+		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Grep string under cursor in cwd" })
+		keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Search vim help tags" })
+		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Search todos" })
 		keymap.set("n", "<leader>fn", "<cmd>Telescope notify<cr>", { desc = "Search notifications" })
 	end,
 }
