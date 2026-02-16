20475b47a18eeeca5a313ba32af01e0b4268d0b4
Anthony M <38088724+Creeperman1524@users.noreply.github.com>
2026-02-16 04:22:29 -0500

    Added nvim which-key summaries
    Added nvim which-key summaries for the "groups" of keys that I have
    setup

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -8,4 +8,18 @@ return {
 	opts = {
 		preset = "modern",
 	},
+	config = function(_, opts)
+		local wk = require("which-key")
+		wk.setup(opts)
+		wk.add({
+			{ "<leader>d", group = "debugger" },
+			{ "<leader>e", group = "file explorer" },
+			{ "<leader>f", group = "telescope" },
+			{ "<leader>h", group = "git" },
+			{ "<leader>s", group = "windows" },
+			{ "<leader>t", group = "tabs" },
+			{ "<leader>w", group = "workspace" },
+			{ "<leader>x", group = "trouble" },
+		})
+	end,
 }
